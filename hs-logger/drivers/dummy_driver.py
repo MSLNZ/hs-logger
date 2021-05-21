0471331d1fb06eaf954cf22f7249ffbdf5ea734e
b.sherson <ben.sherson@callaghaninnovation.govt.nz>
2021-05-21 13:42:36 +1200

    Moved file locations & added setup.py.

---
+++ hs-logger/drivers/dummy_driver.py
@@ -0,0 +1,30 @@
+import random
+
+
+class dummy_driver(object):
+
+    def __init__(self, spec):
+        self.spec = spec
+        pass
+
+    def read_instrument(self, op_id):
+        """
+        read instrument s
+        """
+        v = random.random()
+        w = -v
+        return v, w
+
+    def write_instrument(self, message):
+        """
+        write instrument
+        """
+        return "not here"
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
